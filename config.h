572529009aa7d34b61fd22a6f1cf341a5e298e90
Johan-Mi <johanmi@protonmail.com>
2021-06-05 10:07:51 +0200

    Add centered layout

--- config.h
+++ config.h
@@ -49,6 +49,7 @@ static const int resizehints = 0; /* 1 means respect size hints in tiled resizal
 static const Layout layouts[] = {
 	/* symbol		arrange function */
 	{ "🮉 🮁",		tile },	 /* first entry is default */
+	{ "|M|",		centeredmaster },
 	{ "###",		gaplessgrid },
 	{ "🮥🮮 ",		NULL },	 /* no layout function means floating behavior */
 	{ "[M]",		monocle },
